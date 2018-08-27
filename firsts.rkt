9cbaa8558c27a14d1014ed64b58d1432ec93d93e
Yusong Wu <1391530737@qq.com>
2018-08-27 21:05:36 +0800

    Create firsts.rkt

---
+++ firsts.rkt
@@ -0,0 +1,7 @@
+;; l is the list of lists
+;; this function takes the first s-expression of each list in l
+;; and construct a new list from them
+(define (firsts l)
+  (cond
+  ((null? l)(quote()))
+  (else(cons (car(car l)) (firsts (cdr l))))))
