7388c4d03816615af992a75764b98350a13f7d77
Yusong Wu <1391530737@qq.com>
2018-08-28 19:37:32 +0800

    Create subst2.rkt

---
+++ subst2.rkt
@@ -0,0 +1,9 @@
+;; another subst
+(define (subst2 new o1 o2 lat)
+  (cond ((null? lat)(quote ()))
+        (else (cond
+                ((eq? (car lat)o1)(cons new (cdr lat)))
+                ((eq? (car lat)o2)(cons new (cdr lat)))
+                (else(cons(car lat)
+                          (subst2 new o1 o2
+                                  (cdr lat))))))))
