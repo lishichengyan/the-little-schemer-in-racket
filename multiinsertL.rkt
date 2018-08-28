dc643976b569afc92e182f92cda28a395adf246f
Yusong Wu <1391530737@qq.com>
2018-08-28 21:43:16 +0800

    Create multiinsertL.rkt

---
+++ multiinsertL.rkt
@@ -0,0 +1,14 @@
+;; multiinsertL
+(define (multiinsertL new old lat)
+  (cond
+    ((null? lat)(quote ()))
+    (else
+     (cond
+       ((eq? (car lat)old)
+        (cons new
+              (cons old
+                    (multiinsertL new old
+                                  (cdr lat)))))
+       (else (cons (car lat)
+                   (multiinsertL new old
+                                 (cdr lat))))))))
