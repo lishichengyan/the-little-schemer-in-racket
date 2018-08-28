2b6da2a62858e0edf16f8031db311d32e60478ee
Yusong Wu <1391530737@qq.com>
2018-08-28 19:58:15 +0800

    Create multirember.rkt

---
+++ multirember.rkt
@@ -0,0 +1,11 @@
+;; delete all a's in lat
+;; 如果找到了和a相同的原子，就继续递归调用扫除其他原子
+(define (multirember a lat)
+  (cond ((null? lat)(quote ()))
+        (else
+         (cond
+           ((eq? (car lat)a)
+            (multirember a (cdr lat)))
+           (else(cons (car lat)
+                      (multirember a
+                                   (cdr lat))))))))
