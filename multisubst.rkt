818a85d00c183ac3834b65b87e44efbc6e538ef1
Yusong Wu <1391530737@qq.com>
2018-08-28 21:59:04 +0800

    Create multisubst.rkt

---
+++ multisubst.rkt
@@ -0,0 +1,10 @@
+;; multisubst
+(define (multisubst new old lat)
+  (cond
+    ((null? lat)(quote ()))
+        (else (cond
+                ((eq? (car lat)old)
+                 (cons new (multisubst new old (cdr lat))))
+                (else (cons (car lat)
+                            (multisubst new old
+                                        (cdr lat))))))))
