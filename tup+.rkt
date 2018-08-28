1771e306aa53216f4fcc7ff6ba8537867d6a4c80
Yusong Wu <1391530737@qq.com>
2018-08-28 23:38:55 +0800

    Create tup+.rkt

---
+++ tup+.rkt
@@ -0,0 +1,9 @@
+;; plus two tuples
+(define (tup+ tup1 tup2)
+  (cond
+    ((and (null? tup1)(null? tup2))
+        (quote()))
+        (else
+         (cons (o+ (car tup1)(car tup2))
+               (tup+
+                (cdr tup1)(cdr tup2))))))
