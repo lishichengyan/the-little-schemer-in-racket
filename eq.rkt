49795fbecc4204d286694d82054d5e925ad48a1d
Yusong Wu <1391530737@qq.com>
2019-02-23 19:52:18 +0800

    Update eq.rkt

--- eq.rkt
+++ eq.rkt
@@ -1,4 +1,5 @@
-;; =
+;; =, for non-negative integer comparison
+;; eq? is a built-in function in Racket
 (define (eq n m)
   (cond ((and (zero? n) (zero? m)))  ;; simply ((zero? n)(zero? m)) is ok
         ((zero? n)#f)
