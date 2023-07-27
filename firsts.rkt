f0a22d188f223db2ab75382ba288cc428190543a
Yusong Wu <1391530737@qq.com>
2023-07-26 20:58:53 -0700

    Update firsts.rkt

--- firsts.rkt
+++ firsts.rkt
@@ -1,6 +1,6 @@
 ;; l is the list of lists
 ;; this function takes the first s-expression of each list in l
-;; and construct a new list from them
+;; and constructs a new list from them
 (define (firsts l)
   (cond
   ((null? l)(quote()))
